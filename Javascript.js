71da3b3f3bb4bb91794372694114cc50f31f071e
lilbrat979 <112998849+lilbrat979@users.noreply.github.com>
2022-09-18 11:36:06 -0700

    Add real data to forecast

--- Javascript.js
+++ Javascript.js
@@ -35,26 +35,28 @@ if (minutes < 10) {
 let h2 = document.querySelector("h2");
 h2.innerHTML = `${day}, ${month} ${dates}, ${year} ${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.daily;
   let forecastHTML = `<li class="list-group-item flex-fill list-group-horizontal">`;
-  let days = [
-    "Saturday",
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-  ];
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `<li class="list-group-item flex-fill list-group-horizontal">
-          ${day} <br />
-          ☀
+          ${formatDay(forecastDay.dt)} <br />
+          <img src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"/>
           <br />
-          100°F
+          ${Math.round(forecastDay.temp.max)}°F
         </li>
 `;
   });
@@ -62,10 +64,10 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</li>`;
   forecastElement.innerHTML = forecastHTML;
 }
-function getForecast(coordinates){
-  let apiKey = "420912047755e5beec4a2e60dbb2cbab";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`
-  axios.get(aipUrl).then(displayForecast);
+function getForecast(coordinates) {
+  let apiKey = "6c5621af472ccc1d447bcf74c7a52dd4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 function showTemperature(response) {
   document.querySelector("h1").innerHTML = response.data.name;
@@ -86,7 +88,7 @@ function showTemperature(response) {
     );
   fahrenheitTemperature = response.data.main.temp;
 
-  getForecast(response.data.coord)
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -140,5 +142,3 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 searchCity("Las Vegas");
-
-
